43a328985977b77bd81e1db48f38b613805a763b
hakimix <mustafa.hakimi@outlook.dk>
2022-04-12 19:36:11 +0200

    implement worker

---
+++ worker/index.js
@@ -0,0 +1,31 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000 // if connection is lost, attempt to reconnect once every 1000ms
+});
+
+const sub = redisClient.duplicate();
+
+/**
+ * Calculate fibonacci number
+ * @param  {number} index
+ */
+function fib(index) {
+  if (index > 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+/**
+ * every time a new number shows up in redis, calculate the fibonacci value
+ * and then insert that into a hash called 'values' and store it in Redis. T
+ * The key (message) will be the index
+ * */
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)))
+});
+
+// Subsribe to insert events in Redis.
+sub.subscribe('insert');
---
+++ worker/keys.js
@@ -0,0 +1,5 @@
+// Redis connection keys
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT
+};
