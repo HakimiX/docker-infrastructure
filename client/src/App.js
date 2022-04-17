02bc9e9030b8c36a6bc5b871bae7140ca25c44de
hakimix <mustafa.hakimi@outlook.dk>
2022-04-17 17:38:15 +0200

    change heading

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
+          <h3>Welcome</h3>
           <Route exact path="/" component={Fib} />
           <Route path="/otherpage" component={OtherPage} />
         </div>
